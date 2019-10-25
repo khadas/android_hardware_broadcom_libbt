a1eeed1f6c6520160aabccd23bc6d9210968483e
Wu Liangqing <wlq@rock-chips.com>
2019-10-25 16:52:42 +0800

    sync rockchip code
    Change-Id: Id044f3b6103b9d3aed5b81500e2ee7e587fa2325

--- Android.mk
+++ Android.mk
@@ -63,4 +63,19 @@ include $(LOCAL_PATH)/vnd_buildcfg.mk
 
 include $(BUILD_SHARED_LIBRARY)
 
+ifeq ($(TARGET_PRODUCT), full_maguro)
+    include $(LOCAL_PATH)/conf/samsung/maguro/Android.mk
+endif
+ifeq ($(TARGET_PRODUCT), full_crespo)
+    include $(LOCAL_PATH)/conf/samsung/crespo/Android.mk
+endif
+ifeq ($(TARGET_PRODUCT), full_crespo4g)
+    include $(LOCAL_PATH)/conf/samsung/crespo4g/Android.mk
+endif
+ifeq ($(TARGET_PRODUCT), full_wingray)
+    include $(LOCAL_PATH)/conf/moto/wingray/Android.mk
+endif
+
+    include $(LOCAL_PATH)/conf/rockchip/rksdk/Android.mk
+
 endif # BOARD_HAVE_BLUETOOTH_BCM
